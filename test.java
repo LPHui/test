aec9fc8b7dc9a66fe7bbcdf4a569ee623db2dc05
LPHui <33777756+LPHui@users.noreply.github.com>
2018-06-07 20:06:56 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,10 @@
 class test{
   public static void main(String args[]){
     System.out.pritln("大家好");
-  
+    String a ="12324";
+    String b = "sssss";
+    
+    System.out.println(a+b);
   }
 
 }
